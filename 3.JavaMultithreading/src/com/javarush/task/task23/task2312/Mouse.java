9b5ba6b5ceb337df00c542648b1426cc4aa1ffa5
yubazh <bazhenovyuriy@mail.ru>
2020-09-14 03:23:57 +0300

    adding 14.09.2020

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task23.task2312;
 
 public class Mouse {
-    private int x, y;
+    private int x;
+    private int y;
 
     public Mouse(int x, int y) {
         this.x = x;
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeDirection.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task23.task2312;
 
+
 public enum SnakeDirection {
     UP,
-    DOWN,
     RIGHT,
+    DOWN,
     LEFT
 }
